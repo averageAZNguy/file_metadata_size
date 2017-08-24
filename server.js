2f2e5313021d351ea6147d04ad8c45c8c4599856
averageAZNguy <averageAZNguy@gmail.com>
2017-08-24 15:39:31 -0500

    add css

--- server.js
+++ server.js
@@ -8,16 +8,18 @@ var express = require('express'),
 
 // APP CONFIGURE 
 app.set('view engine','ejs');
+app.use(express.static('css'));
 app.use(bodyparser.urlencoded({extended: true}));
 
+// MAIN PAGE
 app.get('/',function(req,res){
 	res.render('index');
 });
-
+// GET SIZE ROUTE
 app.get('/size',function(req,res){
 	res.send({"size" : size})
 });
-
+// UPLOAD FILE ROUTE
 app.post('/new', upload.single('myfile'), function (req, res, next) {
  	size = req.file.size;
  	res.redirect('/size');
